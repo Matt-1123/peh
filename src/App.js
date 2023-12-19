7b2db30173485ab21eaa6158db3e4095aefc5454
Matt Russo <matthew.russo91@gmail.com>
2023-12-19 10:55:10 -0500

    update storage methods from amplify-cli v5 to v6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 // import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { generateClient, Storage } from "aws-amplify/api";
+import { uploadData, getUrl, remove } from "aws-amplify/storage";
+import { generateClient } from "aws-amplify/api";
 import {
   Button,
   Flex,
@@ -34,7 +35,9 @@ const App = ({ signOut }) => {
     await Promise.all(
       notesFromAPI.map(async (note) => {
         if (note.image) {
-          const url = await Storage.get(note.name);
+          // const url = await Storage.get(note.name);
+          // ^ v6 of amplify-cli uses new methods below
+          const url = await getUrl({ key: note.id });
           note.image = url;
         }
         return note;
@@ -43,30 +46,93 @@ const App = ({ signOut }) => {
     setNotes(notesFromAPI);
   }
 
-  // This function also uses the API class to send a mutation to the GraphQL API. The main difference is that in this function we are passing in the variables needed for a GraphQL mutation so that we can create a new note with the form data.
   async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
-    const image = form.get("Image");
+    const image = form.get("image");
     const data = {
       name: form.get("name"),
       description: form.get("description"),
       image: image.name,
     };
-    if (!!data.image) await Storage.put(data.name, image);
-    await client.graphql({
+    const result = await client.graphql({
       query: createNoteMutation,
       variables: { input: data },
     });
+    if (!!data.image)
+      await uploadData({ key: result.data.createNote.id, data: image }).result;
     fetchNotes();
     event.target.reset();
   }
+
+  // This function also uses the API class to send a mutation to the GraphQL API. The main difference is that in this function we are passing in the variables needed for a GraphQL mutation so that we can create a new note with the form data.
+  // async function createNote(event) {
+  //   event.preventDefault();
+  //   const form = new FormData(event.target);
+  //   const image = form.get("Image");
+  //   const data = {
+  //     name: form.get("name"),
+  //     description: form.get("description"),
+  //     image: image.name,
+  //   };
+
+  //   const result = await uploadData({
+  //     key: data.name,
+  //     data: image,
+  //     options: {
+  //       onProgress: ({ transferredBytes, totalBytes }) => {
+  //         if (totalBytes) {
+  //           console.log(
+  //             `Upload progress ${
+  //               Math.round(transferredBytes / totalBytes) * 100
+  //             } %`
+  //           );
+  //         }
+  //       },
+  //     },
+  //   }).result;
+  //   console.log("Succeeded: ", result);
+
+  // if (!!data.image) await Storage.put(data.name, image);
+  // ^ deprecated v5 amplify-cli
+  // try {
+  //   const result = await uploadData({
+  //     key: data.name,
+  //     data: image,
+  //     options: {
+  //       onProgress: ({ transferredBytes, totalBytes }) => {
+  //         if (totalBytes) {
+  //           console.log(
+  //             `Upload progress ${
+  //               Math.round(transferredBytes / totalBytes) * 100
+  //             } %`
+  //           );
+  //         }
+  //       },
+  //     },
+  //   }).result;
+  //   console.log("Succeeded: ", result);
+  // } catch (error) {
+  //   console.log("Error : ", error);
+  // }
+
+  // TODO: add logic to only upload image if one is selected
+  // if (!!data.image) {}
+  //   await client.graphql({
+  //     query: createNoteMutation,
+  //     variables: { input: data },
+  //   });
+  //   fetchNotes();
+  //   event.target.reset();
+  // }
 
   // this function is sending a GraphQL mutation along with some variables, but instead of creating a note, we are deleting a note.
   async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
-    await Storage.remove(name);
+    // await Storage.remove(name);
+    // ^ amplify-cli v6 uses new method below
+    await remove({ key: id });
     await client.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
